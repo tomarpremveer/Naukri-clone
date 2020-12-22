b296e19716417d53d8470b4f7277dae69a2b8b80
tomarpremveer <montytomar78@gmail.com>
2020-12-22 14:39:42 +0530

    files updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web : node db.js
+web:node db.js
--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const CONNECTIONSTRIN =
   "mongodb+srv://todoapp:8268@cluster0-ygbdi.mongodb.net/naukri?retryWrites=true&w=majority";
 const CONNECTIONSTRING =
   "mongodb://localhost:27017/naukri?retryWrites=true&w=majority";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 mongodb.connect(
   CONNECTIONSTRIN,
   { useNewUrlParser: true, useUnifiedTopology: true },
