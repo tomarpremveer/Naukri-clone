66f8c96058110a4a2aee7886a39128a9634963ce
tomarpremveer <montytomar78@gmail.com>
2020-12-22 14:30:31 +0530

    mongodb url update

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongodb = require("mongodb");
+const CONNECTIONSTRIN =
+  "mongodb+srv://todoapp:8268@cluster0-ygbdi.mongodb.net/naukri?retryWrites=true&w=majority";
 const CONNECTIONSTRING =
   "mongodb://localhost:27017/naukri?retryWrites=true&w=majority";
 const PORT = 3000;
 mongodb.connect(
-  CONNECTIONSTRING,
+  CONNECTIONSTRIN,
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (err, client) {
     module.exports = client;
